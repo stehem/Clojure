4865b11927e9461b3eb4804dead438fed55f27e5
stehem <stehem@gmail.com>
2012-01-14 12:22:53 +0100

    getting there getting there

--- Facebook Enginnering Puzzles/bincrash.clj
+++ Facebook Enginnering Puzzles/bincrash.clj
@@ -7,29 +7,28 @@
 (ns clojure.test.bin-crash
   (:use clojure.test))
 
+
 (defn to-list
   [string]
-  (reverse
-    (map #(read-string %)
-      (remove #(clojure.string/blank? %)
-        (reduce 
-          (fn[memo f] (cons f memo)) (list) (clojure.string/split string #"\s|\t+")
-        )
-      )
-    )
+  (->> 
+    (reduce 
+      (fn[memo f] (cons f memo)) (list) (clojure.string/split string #"\s|\t+")
+    )   
+    (remove #(clojure.string/blank? %))
+    (map #(read-string %))
+    (reverse)
   )
 )
 
 
 (def lines
-  ; hardcoded filename for now
-  (with-open [rdr (clojure.java.io/reader "bincrash.txt")]
-    (reverse
-      (reduce 
-        (fn[memo f] (cons (to-list f) memo)) (list) (rest (line-seq rdr))
-      )
+  (->>
+    (reduce 
+      (fn[memo f] (cons (to-list f) memo)) (list) (rest (line-seq rdr))
     )
-  )
+    (reverse)
+    (with-open [rdr (clojure.java.io/reader "bincrash.txt")])
+  )  
 )
 
 
@@ -46,22 +45,66 @@
   )
 )
 
+(def prices-by-100
+  (reduce
+    (fn[memo f] (merge {(nth f 1) (* 100 (/ (nth f 2) (nth f 1)))} memo)) {} lines
+  ) 
+)
+
+(def sorted-prices-by-100
+  (into 
+    (sorted-map-by (fn [k1 k2] (compare (get prices-by-100 k1) (get prices-by-100 k2)))) prices-by-100
+  )  
+)
+
+(def sorted-without-above
+   (let [m sorted-prices-by-100]
+  (select-keys m (for [[k v] m :when (< k weight-to-drop)] k))) 
+)
+
+; add the cheapest per 100 while under weight to drop
+
+
+(defn dump-manifest
+  [sum cheapest]
+  (if (> (reduce + (concat sum cheapest)) weight-to-drop)
+    sum
+    (recur (concat sum cheapest) cheapest)
+  )    
+)
+
 
 
 (println weight-price-hash)
-(println (get weight-price-hash 75))
+(println prices-by-100)
+(println sorted-prices-by-100)
+(println sorted-without-above)
+(println (dump-manifest (list) (list 700)))
 
+; tests
 
 (deftest check-weight-to-drop
   (is (= 1250 weight-to-drop))         
 )
-
 
 (deftest check-weight-price-hash
   (is (= 3250 (get weight-price-hash 200)))         
   (is (= 10500 (get weight-price-hash 1300)))         
   (is (= 4750 (get weight-price-hash 700)))         
   (is (= 10250 (get weight-price-hash 75)))         
+)
+
+(deftest check-prices-by-100
+  (is (= 1625 (get prices-by-100 200)))         
+)
+
+(deftest check-sorted-prices-by-100
+  (is (= [700 4750/7] (first sorted-prices-by-100)))         
+  (is (= [75 41000/3] (last sorted-prices-by-100)))         
+)
+
+(deftest check-prices-without-above
+  (is (= nil (get sorted-without-above 1300)))         
 )
 
 (run-all-tests #"clojure.test.bin-crash")
