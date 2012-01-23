cc9bc71f7cbe8c51ccf88c8ca32319fb452018e3
Your Name <stehem@gmail.com>
2012-01-23 22:06:12 +0100

    some fixes

--- Facebook Enginnering Puzzles/bincrash.clj
+++ Facebook Enginnering Puzzles/bincrash.clj
@@ -31,13 +31,13 @@
       (fn[memo f] (cons (to-list f) memo)) (list) (rest (line-seq rdr))
     )
     (reverse)
-    (with-open [rdr (clojure.java.io/reader "bincrash.txt")])
+    (with-open [rdr (clojure.java.io/reader "test.in")])
   )  
 )
 
 
 (def weight-to-drop
-  (with-open [rdr (clojure.java.io/reader "bincrash.txt")]
+  (with-open [rdr (clojure.java.io/reader "test.in")]
     (Integer/parseInt(clojure.string/trim-newline (first (line-seq rdr))))
   )
 )
@@ -49,9 +49,11 @@
   )
 )
 
+
 (def prices-by-100
   (reduce
-    (fn[memo f] (merge {(nth f 1) (* 100 (/ (nth f 2) (nth f 1)))} memo)) {} lines
+    (fn[memo f] (merge {(nth f 1) (* 100 (/ (nth f 2) (nth f 1)))} memo)) {}
+    (filter #(> weight-to-drop (second %)) lines) 
   ) 
 )
 
@@ -85,8 +87,9 @@
   )
 )
 
-(def sorted-remaining
-  (sorter (remaining (list cheapest-by-100)))
+(defn sorted-remaining
+  [sum]
+  (sorter (remaining sum))
 )
 
 (defn map-prices 
@@ -98,10 +101,12 @@
 
 (defn dump-manifest
   [sum cheapest]
-  (if (> (reduce + (concat sum cheapest)) weight-to-drop)
-    (concat (map-prices sum) (list (get (first sorted-remaining) 1)))
+  (if (<= weight-to-drop (get (first sorted-prices) 0))
+    (map-prices (list (get (first sorted-prices) 0)))
+  (if (>= (reduce + (concat sum cheapest)) weight-to-drop)
+    (concat (map-prices sum) (list (get (first (sorted-remaining sum)) 1)))
     (recur (concat sum cheapest) cheapest)
-  )    
+  ))
 )
 
 (def dump-manifest-price
@@ -109,37 +114,8 @@
 )
 
 
+
 (println dump-manifest-price)
 
-; tests
-
-
-(deftest check-weight-price-hash
-  (is (= 3250 (get weight-price-hash 200)))         
-  (is (= 10500 (get weight-price-hash 1300)))         
-  (is (= 4750 (get weight-price-hash 700)))         
-  (is (= 10250 (get weight-price-hash 75)))         
-)
-
-(deftest check-prices-by-100
-  (is (= 1625 (get prices-by-100 200)))         
-)
-
-(deftest check-sorted-prices-by-100
-  (is (= [700 4750/7] (first sorted-prices-by-100)))         
-  (is (= [75 41000/3] (last sorted-prices-by-100)))         
-)
-
-
-(deftest check-remaining
-  (is (= 82000 (get (remaining (list 700)) 75)))         
-  (is (= 4750 (get (remaining (list 700)) 700)))         
-  (is (= 9750 (get (remaining (list 700)) 200)))         
-  (is (= 10500 (get (remaining (list 700)) 1300)))         
-)
-
-(deftest this-thing-work
-  (is (= 9500 dump-manifest-price))         
-)
 
 (run-all-tests #"clojure.test.bin-crash")
