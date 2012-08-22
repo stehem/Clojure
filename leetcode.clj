a21f2d6599a32188c1ad695bd7e7e9eb2dd520b1
stephane <stephane@stephane-VPCYB1S1E.(none)>
2012-08-22 19:31:24 +0200

    combination algorithm

--- leetcode.clj
+++ leetcode.clj
@@ -31,7 +31,7 @@
   [board queen]
   (map 
     (fn[cell] 
-      (if (or (= 'Q cell) (= 'X cell))
+      (if (not (vector? cell))
         cell
         (let [[xcell ycell] cell [xqueen yqueen] queen diffx (- xqueen xcell) diffy (- yqueen ycell)]
           (cond
@@ -49,7 +49,7 @@
   [boards]
   (reduce 
     (fn[result board] 
-      (into result (map (fn[queen] (add-queen board queen)) (filter #(and (not= % 'Q) (not= % 'X)) board))))
+      (into result (map (fn[queen] (add-queen board queen)) (filter #(vector? %) board))))
         (set nil) boards))
 
 
@@ -78,13 +78,15 @@
     (is (= 2 (count (nqueens 4))))
     (is (= 10 (count (nqueens 5))))
     (is (= 4 (count (nqueens 6))))
-    (is (= 40 (count (nqueens 7))))
-    (is (= 92 (count (nqueens 8)))))
+    ;(is (= 40 (count (nqueens 7))))
+    ;(is (= 92 (count (nqueens 8))))
+         )
 ; /N Queens
 
 
 ; Next Permutation
 ; did that already for Project Euler, pretty much copy/paste
+; OLD CODE, PREFER REDUCE OVER LOOP RECUR => MORE IDIOMATIC
 (defn find-k
   [coll]
     (loop [k (- (count coll) 2)]
@@ -157,20 +159,72 @@
 
 
 
-; Permutations
+; Permutations 1&2
 (defn permutations
   [grp]
-  
-  
-  
-)
-; /Permutations
+  (loop [i 2 perm (lexi-perms grp i) perms [grp perm]]
+    (if (= (last perms) (last (butlast perms)))
+      (butlast perms)
+        (let [ii (inc i) next-perm (lexi-perms grp ii)]
+          (recur ii next-perm (conj perms next-perm))))))
+
+(deftest test-permutations
+  (is '([1 1 2] [1 2 1] [2 1 1]) (permutations [1 1 2]))
+  (is '([1 2 3] [1 3 2] [2 1 3] [2 3 1] [3 1 2] [3 2 1]) (permutations [1 2 3])))
+; /Permutations 1&2
 
 
+;homemade combination algorithm, way more fun than using clojure.contrib :-)
+(defn combine
+  [base n]
+  (reduce 
+    (fn[acc x]  
+      (into acc (map (fn[y] (conj x y)) (range (inc (last x)) n))))
+    [] base))
 
 
+(defn map-indices-to-value
+  [combs grp]
+    (map (fn[comb] (map (fn[idx] (get grp idx)) comb)) combs))
 
 
+(defn combinations
+  [grp c]
+  (let [n (count grp) start (map (fn[i] [i]) (range 0 n))]
+    (map-indices-to-value
+      (reduce (fn[acc x] (combine acc n)) start (range 1 c))
+      grp)))
+
+
+(deftest test-combinations 
+  (is (=
+       '(
+         (a b c) (a b d) (a b e) (a b f) (a b g) (a b h) (a c d) (a c e) (a c f) (a c g) 
+         (a c h) (a d e) (a d f) (a d g) (a d h) (a e f) (a e g) (a e h) (a f g) (a f h) 
+         (a g h) (b c d) (b c e) (b c f) (b c g) (b c h) (b d e) (b d f) (b d g) (b d h) 
+         (b e f) (b e g) (b e h) (b f g) (b f h) (b g h) (c d e) (c d f) (c d g) (c d h) 
+         (c e f) (c e g) (c e h) (c f g) (c f h) (c g h) (d e f) (d e g) (d e h) (d f g) 
+         (d f h) (d g h) (e f g) (e f h) (e g h) (f g h) 
+        ) (combinations '[a b c d e f g h]  3)))
+  (is (= 28 (count (combinations '[a b c d e f g h]  2))))
+  (is (= 70 (count (combinations '[a b c d e f g h]  4))))
+  (is (= 56 (count (combinations '[a b c d e f g h]  5))))
+  (is (= 28 (count (combinations '[a b c d e f g h]  6))))
+  (is (= 8 (count (combinations '[a b c d e f g h]  7)))))
+  ;about 6 secs for generating 3,5M combinations there is room for improvement also INTO IS COSTLY!!!
+  ;(time (combinations '[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]  9)) 
+; /homemade combination algorithm
+
+
+;Subsets
+(defn subsets
+  [grp]
+  (reduce (fn[acc x] (apply conj acc (combinations grp x))) ['()] (range 1 (inc (count grp)))))
+
+
+(deftest test-subsets
+  (is (= '[() (1) (2) (3) (1 2) (1 3) (2 3) (1 2 3)] (subsets [1 2 3]))))
+; /Subsets
 
 
 (run-all-tests #"clojure.test.leetcode")
